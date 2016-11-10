04f8d83a485c038aec4fd243b93ea229efee593a
Marc Cooperstein <mcooperstein@gmail.com>
2016-11-09 20:40:16 -0800

    added words to arrays

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     $("p").hide();
     $("#reset").hide();
-    var a = ["smelly", "hairy", "ugly", "fat", "manly"];
-    var b = ["eating", "pooping", "drinking", "licking", "sniffing", "sleeping with"];
-    var c = ["poop", "trash", "squirrels", "play-doh"];
+    var a = ["smelly", "hairy", "ugly", "fat", "manly", "dirty", "slimy"];
+    var b = ["eating", "pooping", "drinking", "licking", "sniffing", "sleeping with", "groping", "assaulting"];
+    var c = ["poop", "trash", "squirrels", "play-doh", "sewer rats"];
 
-    var adjective = a[Math.floor(Math.random() * 5)];
-    var verb = b[Math.floor(Math.random() * 6)];
-    var noun = c[Math.floor(Math.random() * 4)];
+    var adjective = a[Math.floor(Math.random() * 7)];
+    var verb = b[Math.floor(Math.random() * 8)];
+    var noun = c[Math.floor(Math.random() * 5)];
 
     var insult = "'s mom is " + adjective + " and likes " + verb + " " + noun + "!!!";
 
